862d07e6de3f4cb066410cd41e6fda6e2d9944c7
Ethan Chen <intervigil@gmail.com>
2017-09-16 23:19:53 -0700

    msm8916-common: Add vibrator HAL in place of CMHW implementation
    Change-Id: I19d4f916d807301ec30b882bfdeaf76715a041c7

--- msm8916.mk
+++ msm8916.mk
@@ -232,7 +232,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.bq_8916
 
 # Wifi
 PRODUCT_PACKAGES += \
