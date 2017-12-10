ea3965bb84a1b53efd1ae0144171966bce84adb6
Bruno Martins <bgcngm@gmail.com>
2017-12-10 23:23:40 +0000

    msm8916-common: init: Update extension for O-mr1
    Change-Id: I342326ed306e919f458ff6715bf3130844ff74f4

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := \
     system/core/base/include \
-    system/core/init
+    system/core/init \
+    external/selinux/libselinux/include \
+    external/libcap/libcap/include
 
 LOCAL_CFLAGS := -Wall
 LOCAL_SRC_FILES := init_msm8916.cpp
