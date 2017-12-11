b0f8ed05fe00436ac3c0834fb666121d89829a6f
Aymende7 <aymenbousselham@gmail.com>
2017-12-11 17:51:00 +0000

    msm8916-common: Disable obsolete global cflags
    Change-Id: I6452915220fe7e03969adf18f5076e96376c9717

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,9 +30,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_BASE := 0x80000000
