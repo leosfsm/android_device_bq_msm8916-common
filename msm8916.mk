afe609fa4f3502db71c588787361873106d02ede
aymende7 <aymenbousselham@gmail.com>
2018-01-15 23:09:20 +0000

    msm8916-common: Convert audio_effects.conf to XML
    * Converted .conf format to .xml using aeffects-conf2xml

--- msm8916.mk
+++ msm8916.mk
@@ -52,7 +52,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/etc/audio_effects.xml
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
