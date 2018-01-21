9c1be2cfaf846fdadfc3740ce60b18538b6917bd
aymende7 <aymenbousselham@gmail.com>
2018-01-21 13:57:45 +0000

    msm8916-common: Import audio_effects.conf from CAF
    * Stolen from https://github.com/LineageOS/android_hardware_qcom_audio/commit/78767a8abba54727f61becee6c0056d0c0400b71#diff-e5bf76fa01b77bd186203ed8a589c73b
    
    * Also move audio_policy_configuration.xml to audio folder
    
    Change-Id: I78dd55a97bef9bda72bc918f8216fa7453f463ac

--- msm8916.mk
+++ msm8916.mk
@@ -46,13 +46,13 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml \
-    hardware/qcom/audio-caf/msm8916/configs/msm8916_32/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
