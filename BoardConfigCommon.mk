d2f9201f3ae6fc63416b8e158acaaa01277791a9
aymende7 <aymenbousselham@gmail.com>
2018-02-26 18:43:42 +0000

    msm8916-common: Include legacy qcom sepolicy
    Change-Id: Id3643a189d0ba96ff29d7d34bdf6235df4a3b699

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -136,6 +136,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/bq/msm8916-common/sepolicy
 
 # Shims
