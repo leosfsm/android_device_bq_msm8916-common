3133a8d1499fa49b0ee81649508c6cfab230a045
aymende7 <aymenbousselham@gmail.com>
2018-06-21 16:55:06 +0100

    msm8916-common: Remove LOCAL_32_BIT_ONLY

--- camera/Android.mk
+++ camera/Android.mk
@@ -22,7 +22,6 @@ LOCAL_SRC_FILES := CameraParameters.cpp
 LOCAL_MODULE := libcamera_parameters_shim
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
-LOCAL_32_BIT_ONLY := true
 LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
