2b23aa08d10f4734c7d907daf8af1f6a923a59cd
leosfsm <leo.sfsm@gmail.com>
2019-03-20 18:13:04 +0100

    msm8916-common: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>

--- msm8916.mk
+++ msm8916.mk
@@ -244,6 +244,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Usb
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.bq_8916
