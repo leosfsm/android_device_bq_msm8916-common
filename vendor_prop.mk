7470df35351ead394ef2514f274b2b657e1c92f5
dianlujitao <dianlujitao@lineageos.org>
2019-03-20 14:10:52 +0100

    msm8916-common: Rename bt props for new HAL
    Change-Id: I1afd110bffed0f43b1afcbef99be8a3776d24e0b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -21,7 +21,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    qcom.bluetooth.soc=smd \
+    vendor.qcom.bluetooth.soc=pronto \
     ro.bluetooth.hfp.ver=1.7 \
     ro.qualcomm.bt.hci_transport=smd \
     ro.bluetooth.dun=true \
