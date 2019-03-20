677d8188e808a1fddf630da7718ad94139524300
Michael Bestas <mkbestas@lineageos.org>
2019-03-20 17:47:31 +0100

    msm8916-common: Stop copying WCNSS_qcom_cfg.ini to userdata
    Change-Id: I7b9fbc0e26a1563f3e13fe0f9d58308b618a5702

--- Android.mk
+++ Android.mk
@@ -135,11 +135,4 @@ $(WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(WIDEVINE_SYMLINKS)
 
-WCNSS_CFG_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
-$(WCNSS_CFG_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf /data/vendor/wifi/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CFG_SYMLINK)
-
 endif
